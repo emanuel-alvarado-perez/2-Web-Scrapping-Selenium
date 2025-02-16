ca46c4701af315817f96c641cdb6d6a015b5ef8e
emanuel-alvarado-perez <emanuel.alvarado.perez@outlook.com>
2025-02-16 03:19:11 +0000

    Ejercicio1

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 #from selenium.webdriver.chrome.service import Service
 #service = Service('C:\chromedriver.exe')
 import time
+from selenium.webdriver.common.keys import Keys
 
 def get_driver():
   # Configuración de parametros iniciales
@@ -28,7 +29,7 @@ def clean_text(text):
   output=float(text.split(":")[1])
   return output
 
-def main():
+def main_webScraping():
   driver=get_driver()
   time.sleep(2)
   #Buscar un texto especifico
@@ -36,7 +37,78 @@ def main():
   element=driver.find_element("xpath","/html/body/div[1]/div/h1[2]") #Inspect, Copy Full Xpath
   return clean_text(element.text)
 
-print(main())
+"""Ejecucion Primera Parte"""
+#print(main_webScraping())
+
+def get_driver_LoginApp():
+  # Configuración de parametros iniciales
+  options=webdriver.ChromeOptions() #Version Replit
+
+  options.add_argument("disable-infobars") #Deshabilitar msj de info 
+  options.add_argument("start-maximized") #Maximizar pantalla
+  options.add_argument("disable-dev-shm-usage") #Evitar errones linux
+  options.add_argument("no-sandbox") #Mayores permisos en el sitio web
+
+  #Evita que Selenium sea identificado por el Navegador
+  options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
+  options.add_argument("disable-blink-features=AutomationControlled") 
+
+  #Inicializar clase Driver con opciones seteadas
+  driver=webdriver.Chrome(options=options)
+  #driver=webdriver.Chrome(service=service,options=options) Para Local
+  driver.get("http://automated.pythonanywhere.com/login/")
+  return driver
 
 
+def main_LoginApp():
+  driver=get_driver_LoginApp()  
+  driver.find_element(by="id",value="id_username").send_keys("automated")
+  time.sleep(2)
+  driver.find_element(by="id",value="id_password").send_keys("automatedautomated"+Keys.RETURN)
+  time.sleep(2)
+  driver.find_element(by="xpath",value="/html/body/nav/div/a").click()
+  time.sleep(2)
+  print(driver.current_url)
 
+"""Ejercicio Segunda Parte"""
+#print(main_LoginApp())
+
+
+def get_driver_Exercise1():
+  # Configuración de parametros iniciales
+  options=webdriver.ChromeOptions() #Version Replit
+
+  options.add_argument("disable-infobars") #Deshabilitar msj de info 
+  options.add_argument("start-maximized") #Maximizar pantalla
+  options.add_argument("disable-dev-shm-usage") #Evitar errones linux
+  options.add_argument("no-sandbox") #Mayores permisos en el sitio web
+
+  #Evita que Selenium sea identificado por el Navegador
+  options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
+  options.add_argument("disable-blink-features=AutomationControlled") 
+
+  #Inicializar clase Driver con opciones seteadas
+  driver=webdriver.Chrome(options=options)
+  driver.get("http://automated.pythonanywhere.com/login/")
+  return driver
+
+def main_Exercise1():
+  driver=get_driver_LoginApp()  
+
+  # Ingresar credenciales
+  driver.find_element(by="id",value="id_username").send_keys("automated")
+  time.sleep(2)
+  driver.find_element(by="id",value="id_password").send_keys("automatedautomated"+Keys.RETURN)
+  time.sleep(2)
+
+  # Hacer click en el boton de "Home"
+  driver.find_element(by="xpath",value="/html/body/nav/div/a").click()
+  time.sleep(2)
+
+  # Extraer texto de la temperatura
+  temperatura=driver.find_element(by="xpath",value="/html/body/div[1]/div/h1[2]").text
+  return clean_text(temperatura)
+
+
+"""Ejercicio Practico"""
+print(main_Exercise1())
